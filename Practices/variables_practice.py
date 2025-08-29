9f80788e509cb635dc35b019f6bb781e77a9fc4c
kalzcool <nani.carrillo@ucas-edu.net>
2025-08-29 14:42:55 -0600

    variables notes

---
+++ Practices/variables_practice.py
@@ -0,0 +1,15 @@
+print("WEWEWEWEWEWEWEWE ANDY COME BACKKKKKKKKKK")
+
+#examples of variables 
+school = "UCAS"
+single_digit = 4 
+how_much_wood_could_a_woodchuck_chuck = 10 
+
+
+how_much_wood_could_a_woodchuck_chuck = 99999999999999999 
+
+print(how_much_wood_could_a_woodchuck_chuck) 
+
+name = input("What is your name?")
+
+print("hello", name)
