ffe90e9ecf57988e3d6160369ca0c4ff8ce9743e
kalzcool <nani.carrillo@ucas-edu.net>
2025-09-02 14:38:08 -0600

    variable's practice and first program

---
+++ Practices/variables/practice.py
@@ -0,0 +1,45 @@
+#CC_7
+
+name = "calix" 
+
+single_digit = 2 
+
+tripple_digit = 265 
+
+breakfast = "bagel" 
+
+fav_color = "green" 
+
+school = "UCAS"
+
+year_current = 2025 
+
+eye_color = "blue"
+
+age = 14
+
+subject_fav = "art"
+
+print(name)
+
+print(single_digit)
+
+print(tripple_digit)
+
+print(breakfast)
+
+print(fav_color)
+
+print(school)
+
+print(year_current)
+
+print(eye_color)
+
+print(age)
+
+print(subject_fav)
+
+birthday = input("when is your birthday?")
+
+print (birthday ,"HOLY PEAK")
