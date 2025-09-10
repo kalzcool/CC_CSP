9b03bbec333eb5cba9cadc6bd1fcd9eadaa41344
kalzcool <nani.carrillo@ucas-edu.net>
2025-09-10 20:57:02 +0000

    finished madlib

--- Practices/madlib.py
+++ Practices/madlib.py
@@ -1,7 +1,9 @@
 #CC 7th madlib thing 
 
 word1 = input("Give me an adjective:\n").strip().title()
-word2 = input("Give me a verb\n")
-word3 = input("Give me a messy object\n")
-word4 = input("Give me a name\n")
+word2 = input("Give me a verb\n").strip().title()
+word3 = input("Give me a messy object\n").strip().title()
+word4 = input("Give me a name\n").strip().title().capitalize
 
+print(f"The {word1}, boy {word2} through his front door and down the hall. Smearing {word3} on  the walls as he  went.")
+print(f"{word4}! His mother screamed. What in the world do you think you are doing?")
