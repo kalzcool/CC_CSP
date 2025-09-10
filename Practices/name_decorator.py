dbe9c58cffadaa59780567b5f20028c0274be3b1
kalzcool <nani.carrillo@ucas-edu.net>
2025-09-10 14:28:49 -0600

    made name decorator

---
+++ Practices/name_decorator.py
@@ -0,0 +1,9 @@
+#CC 7th, name decorator 
+
+name = input("Hello! What is your name?:\n").strip().title()
+last_name = input("and your last name honey?\n").strip().title() 
+
+full_name = name + " " +  last_name 
+
+print("^^" + full_name + "^^") 
+print("That's a beautiful name honey.")
