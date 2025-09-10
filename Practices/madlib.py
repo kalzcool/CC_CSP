65c6963cc39d4c6133346838550b7a4df502b0a9
kalzcool <nani.carrillo@ucas-edu.net>
2025-09-10 14:42:19 -0600

    halfway madlibs assignment

---
+++ Practices/madlib.py
@@ -0,0 +1,7 @@
+#CC 7th madlib thing 
+
+word1 = input("Give me an adjective:\n").strip().title()
+word2 = input("Give me a verb\n")
+word3 = input("Give me a messy object\n")
+word4 = input("Give me a name\n")
+
