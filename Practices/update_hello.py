45d3eaeff3733e6f2610cc230ad23734f925d096
kalzcool <nani.carrillo@ucas-edu.net>
2025-09-16 14:27:01 -0600

    Updated Hello world

---
+++ Practices/update_hello.py
@@ -0,0 +1,10 @@
+#CC 7th updating first assignment
+
+def hello_user(name):
+    print(f"Hello {name}!") 
+
+hello_user("Ares")
+hello_user("Melissa")
+hello_user("Berrak")
+hello_user("Andy")
+hello_user("Anna")
