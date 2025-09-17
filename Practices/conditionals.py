8080a6e2b94681289b23352074972dd6e0950571
kalzcool <nani.carrillo@ucas-edu.net>
2025-09-17 14:36:34 -0600

    fixed buget calc update, halfway through conditionals notes

---
+++ Practices/conditionals.py
@@ -0,0 +1,16 @@
+#CC 7th conditonals notes
+
+#homework_done = input("is your homework done: ").strip().capitalize()
+
+#if homework_done == "No":
+#    print("Then go do your homework.")
+#else:
+  #  print("Yes you can go")
+
+grade = 100 
+
+if grade >89:
+    print(f"You have {grade}% that is an A :3") 
+if grade > 79:
+else: 
+    print(f"You have a {grade}% that is so far from an A.. How did you even get that.?")
--- Practices/update_budget_calc.py
+++ Practices/update_budget_calc.py
@@ -1,31 +1,20 @@
 #CC 7th updating financial calculator 
 
-def monthly_amounts(income):
-    income = float(input("What is your monthly income:"))
-    rent = float(input("what is your monthly rent:"))
-    utilities = float(input("utilities:"))
-    groceries = float(input("groceries"))
-    transportation = float(input("transportation"))
-    savings = income*.1 
-    spend = income -(utilities +rent +groceries +transportation + savings) 
-    rent_percent = round(rent/income) *100 , 2
-    utilities_percent = round (utilities/income) *100 , 2
-    groceries_percent = round (groceries/income) *100 , 2
-    transportation_percent = round (transportation/income) * 100 , 2
-    savings_percent = round (savings/income) *100,2 
-    spend_percent = round (spend/income) *100,2 
+def monthly_amounts(other_things):
+   return round(other_things/ income*100,2)
+
     
+income= 3000
 
-rent_percent = monthly_amounts(round(rent/income)*100,2)
-utilities_percent = monthly_amounts(round(utilities/income)*100,2)
-groceries_percent = monthly_amounts(round(groceries/income)*100,)
-transportation_percent = monthly_amounts(round(transportation/income)*100,2)
-savings_percent = monthly_amounts(round(savings/income)*100,2)
-spend_percent = monthly_amounts(round(spend/income)*100,2)
+rent_percent = monthly_amounts(1500)
+utilities_percent = monthly_amounts(600)
+groceries_percent = monthly_amounts(100)
+transportation_percent = monthly_amounts(85)
 
-print(f"\nYour rent is  and it is {rent_percent:.2f}% of your income")
-print(f"\nYour utilities is ${utilities:.2f} it is {utilities_percent:.2f}% of your income") 
-print(f"\nYour groceries is ${groceries:.2f} it is {groceries_perecent:.2f}%")
-print(f"\nYour transportation is{transportation:.2f} is it {transportation_percent:.2f}%")
-print(f"\nYour savings are {savings:.2f} it is {savings_percent}%")
-print(f"\nYour amount to spend is {spend:.2f} it is {spend_percent}%") 
+
+print(f"\nYour rent is {rent_percent}% of your income")
+print(f"\nYour utilities  is {utilities_percent}% of your income") 
+print(f"\nYour groceries is {groceries_percent}% of your income")
+print(f"\nYour transportation is it {transportation_percent}% of your income")
+
+#HOLY ANCIENTS I FIGURE IT OUT(HOPEFULLY) I MIGHT ACTUALLY CRY GUYS 
