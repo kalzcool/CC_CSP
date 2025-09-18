21e93817948e6644c55c0c16b726f75ebdab3daa
kalzcool <nani.carrillo@ucas-edu.net>
2025-09-18 14:21:06 -0600

    finished conditionals

--- Practices/conditionals.py
+++ Practices/conditionals.py
@@ -7,12 +7,30 @@
 #else:
   #  print("Yes you can go")
 
-grade = 89
+
+name = input("What is your name?:\n") 
+if name == "Ms LaRose":
+    print("You are the Teacher")
+elif name == "Tia" or name == "Ashley":
+    print("You are the TA")
+else:
+    print(f"Hello {name}, you are a student")
+     
+grade = 157
 
 if grade >=90:
-    print(f"You have {grade}% that is an A :3") 
-elif grade >= 80: 
+    if grade > 100:
+        print("You cheated, that is not possible")
+    else:
+        print(f"You have {grade}% that is an A :3") 
+elif grade >= 80 and not grade > 100: 
     print(f"You have {grade}% that is a B")
-elif grade 
+elif grade >= 70 and not grade > 100:
+    print(f"you have a {grade}% that is a c")
 else: 
     print(f"You have a {grade}% that is so far from an A.. How did you even get that.?")
+
+
+
+
+
