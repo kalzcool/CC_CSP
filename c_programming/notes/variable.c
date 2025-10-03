428788bce9d8b2a2fb574d3ef844a27f1b0d383b
UCAS-kalzkool <nani.carrillo@ucas-edu.net>
2025-10-03 14:42:48 -0600

    variables notes

--- c_programming/notes/variable.c
+++ c_programming/notes/variable.c
@@ -2,12 +2,20 @@
 #include <stdio.h>
 
 int main(void){
-    int grade = 95; //4 bytes
+    int grade; //4 bytes
     float pi = 3.14; //4 bytes
     double long_pi = 3.145926358; //8 bytes of space
-    char letter_grade = 'A'; // 1 byte
+    char letter_grade; // 1 byte
     char name[] = "Calix";
-    printf("%s did it?", name);
+    // user input
+    printf("What is your grade percentage as a whole number: ");
+    scanf("%d", &grade);
+
+    printf("what is your letter grade: ");
+    scanf(" %c", &letter_grade);
+
+
+    printf("%s did it?\n", name);
     printf("You have a %d, in the class. That is an %c", grade, letter_grade);
     return 0;
 }
