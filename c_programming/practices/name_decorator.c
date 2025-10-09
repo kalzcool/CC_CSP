50f31b9ca502f06daf6b5ef2719940975727eac2
UCAS-kalzkool <nani.carrillo@ucas-edu.net>
2025-10-09 14:39:22 -0600

    name decorater done

---
+++ c_programming/practices/name_decorator.c
@@ -0,0 +1,22 @@
+//CC 7th name decorator 
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+
+    char first_name[50];
+    printf("Tell me your first name:\n");
+    scanf("%s", first_name);
+    char last_name[50]; 
+    printf("Tell me your last name:\n");
+    scanf("%s", last_name);
+
+    char full_name[50]; 
+    strcat(full_name, last_name);
+
+    printf("^^%s %s^^", first_name, last_name);
+   
+
+
+    return 0; 
+}
