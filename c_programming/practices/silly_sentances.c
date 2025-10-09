7091cbbbdfa0da6b0bb49364248a48198c3374da
kalzcool <nani.carrillo@ucas-edu.net>
2025-10-09 21:08:54 +0000

    finsihed silly sentences, i think, codespaces hate me for sure

--- c_programming/practices/silly_sentances.c
+++ c_programming/practices/silly_sentances.c
@@ -1,1 +1,24 @@
-//
+//CC 7th silly sentances
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+
+    char name[50];
+    printf("Give me a name:\n");
+    scanf("%s\n", name);
+
+    int num;
+    printf("Give me a number:\n");
+    scanf("%d\n", num);
+
+    char noun[50];
+    printf("Give me a noun:\n"); 
+    scanf("%s\n",noun);
+
+    printf("%s..... why would I ever want to give someone %d %s.That could horrible for their health.\n", name,num, noun);
+
+
+
+    return 0;
+}
