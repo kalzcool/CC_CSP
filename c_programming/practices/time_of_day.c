ba502cf2db6f6569a19a7807f8dff8d3201aef09
kalzcool <nani.carrillo@ucas-edu.net>
2025-10-10 20:59:46 +0000

    hopefully finished time of day

--- c_programming/practices/time_of_day.c
+++ c_programming/practices/time_of_day.c
@@ -1,6 +1,6 @@
 //CC 7th time of day
 #include <stdio.h>
-
+//Codespaces never lets me  run my code, so I am reveiwing this and hoping it runs properly.
 int main(void){
     int time;
 
@@ -10,8 +10,14 @@ int main(void){
     if(time >= 5){
         printf("Go to bed please\n");
     }else if(time <=12 && time>= 6){
-        printf("Good morning")
-    }else if(time >=)
+        printf("Good morning\n");
+    }else if(time >=12 && time<=18){
+        printf("Good afternoon\n");
+    }else if(time >=18 && time <=20){
+        printf("Good evening");
+    }else{
+        printf("Good night")
+    }
 
     return 0;
 }
