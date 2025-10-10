b41782bebbdc59870c36b3e65cb6d4669d4ff2cc
UCAS-kalzkool <nani.carrillo@ucas-edu.net>
2025-10-10 14:41:46 -0600

    almost done with time of day

---
+++ c_programming/practices/time_of_day.c
@@ -0,0 +1,17 @@
+//CC 7th time of day
+#include <stdio.h>
+
+int main(void){
+    int time;
+
+    printf("What is the current military time:\n");
+    scanf("%d", time);
+
+    if(time >= 5){
+        printf("Go to bed please\n");
+    }else if(time <=12 && time>= 6){
+        printf("Good morning")
+    }else if(time >=)
+
+    return 0;
+}
