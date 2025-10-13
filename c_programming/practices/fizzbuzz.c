fe6f0c46707e80e6666e4604f0541f3f0de208c8
kalzcool <nani.carrillo@ucas-edu.net>
2025-10-13 21:00:56 +0000

    probably didn't get fizzbuzz right, codespaces hates me

--- c_programming/practices/fizzbuzz.c
+++ c_programming/practices/fizzbuzz.c
@@ -6,12 +6,12 @@ int main(void){
     int num= 1;
     while(num<51){
         num++;
-        if(num%3){
-            printf("Fizz\n");
+        if(num%15){
+            printf("Fizzbuzz\n");
         }else if(num%5){
             printf("buzz\n");
-        }else if(num%3 && num%5){
-            printf("fizzbuzz\n");
+        }else if(num%3){
+            printf("fizz\n");
         }else{
             printf("%d\n", num);
         } 
