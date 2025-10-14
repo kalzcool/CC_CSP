9f1cfde53846cb92e9499718b620a07f7ee081ab
UCAS-kalzkool <nani.carrillo@ucas-edu.net>
2025-10-14 14:42:15 -0600

    finished update hello

---
+++ c_programming/practices/update_hello_world.c
@@ -0,0 +1,19 @@
+//CC 7th update hello world
+#include <stdio.h>
+
+void greeting(char* name){
+    printf("hello %s, I miss you :[\n", name);
+}
+
+int main(void){
+
+    greeting("Ares");
+    greeting("little miss");
+    greeting("Melissa");
+    greeting("Alex");
+    greeting("Berrak");
+    greeting("Andy");
+    greeting("Anna");
+
+    return 0;
+}
