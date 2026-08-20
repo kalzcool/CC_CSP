0539e7a4f20387bbf155b86757d099ac33da505e
kalzcool <nani.carrillo@ucas-edu.net>
2026-08-20 09:05:09 -0600

    I did not push variables notes

---
+++ cp1/variable_notes.py
@@ -0,0 +1,19 @@
+#cc variable notes
+#first variable
+
+name = "Anna"
+
+print("Hello " + name)
+
+#reset variable
+
+name = "J"
+
+print("sorry I meant " + name)
+
+age= 15
+
+print("Wow, you are ", age)
+print("Oh wait your birthday was yesterday?")
+age +=1 
+print("So you are", age)
