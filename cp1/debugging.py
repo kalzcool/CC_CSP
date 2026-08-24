5f51cc5cf1985ac095e8c5138444dea19cd60a6b
kalzcool <nani.carrillo@ucas-edu.net>
2026-08-24 09:11:32 -0600

    finished debugging assignment

---
+++ cp1/debugging.py
@@ -0,0 +1,8 @@
+#CC debugging fix 
+
+name = "Alex"
+age = 16
+
+print("My name is " + name + " and I am " + str(age) + " years old.")
+#Missed the second parenthese, now added. 
+#When calling name it was mispelled.I fixed the spelling of name 
